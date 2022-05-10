6feb157fad4d25826da2219ab4ca55d78245ddff
Daniel Reollo <dreollo92@gmail.com>
2022-05-10 13:26:37 +0200

    AllReadyToCreate

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, 
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text, 
+  StatusBar,
 
-export default function App() {
+} from 'react-native';
+
+
+const  App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
+
+export default App
