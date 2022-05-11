6ba649e8ac90c554ce159acbf9cae297c9d9678f
Daniel Reollo <dreollo92@gmail.com>
2022-05-11 13:22:48 +0200

    API_Add

--- componentes/Formulario.js
+++ componentes/Formulario.js
@@ -1,16 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
-
+import axios from 'axios';
 
 const Formulario = () => {
 
     const [moneda, guardarMoneda] = useState('');
-    const [cryptomoneda, guardarCryptomoneda] = useState('');
+    const [criptomoneda, guardarCriptomoneda] = useState('');
+    const [criptomonedas, guardarCriptomonedas] = useState([]);
 
+    useEffect(() => {
+        const consultarAPI = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+            const resultado = await axios.get(url);
+            guardarCriptomonedas(resultado.data.Data)
 
+        }
+        consultarAPI(); //la llamamos para que se ejecute.
+    }, []);
+
+    //Almacena las selecciones del usuario
     const obtenerMoneda = moneda => {
-        console.log(moneda)
+        guardarMoneda(moneda)
+    }
+    const obtenerCriptomoneda = cripto => {
+        guardarCriptomoneda(cripto)
     }
 
     return ( 
@@ -18,7 +32,9 @@ const Formulario = () => {
         <View>
             <Text style={styles.label}>Moneda</Text>
             <Picker
+                selectedValue={moneda}
                 onValueChange={ moneda => obtenerMoneda(moneda) }
+                itemStyle={{height: 120 }}
             >
                 <Picker.Item label='- Seleccione -' value={''} />
                 <Picker.Item label='Dolar EE.UU.' value={'USD'} />
@@ -27,9 +43,19 @@ const Formulario = () => {
                 <Picker.Item label='Libra Esterlina' value={'GBP'} />
             </Picker>
             <Text style={styles.label}>Criptomoneda</Text>
+            <Picker
+                selectedValue={criptomoneda}
+                onValueChange={ cripto => obtenerCriptomoneda(cripto) }
+                itemStyle={{height: 120 }}
+            >
+                <Picker.Item label='- Seleccione -' value={''} />
+                {criptomonedas.map( cripto => (
+                    <Picker.Item key={cripto.CoinInfo.Id} label={cripto.CoinInfo.FullName} value={cripto.CoinInfo.Name}/>
+                ))}
+            </Picker>
         </View>
 
-     );
+     ); 
 }
 
 const styles = StyleSheet.create({
